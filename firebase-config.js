f7188b0a76f26c5c731c932b95bbc2e0ca187d31
RAJEHALRABIE <rajehalrabie@gmail.com>
2025-10-23 01:59:55 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,37 +1,37 @@
 // firebase-config.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
-import { 
-  getFirestore, collection, addDoc, updateDoc, doc, getDocs, query, where, getDoc, writeBatch, deleteDoc, orderBy
+import {
+  getFirestore, collection, addDoc, updateDoc, doc,
+  getDocs, query, where, getDoc, writeBatch, deleteDoc, orderBy
 } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
 
-// يجب استبدال هذا التكوين بالتكوين الفعلي الخاص بك
+// إعدادات Firebase الصحيحة من لوحة التحكم
 const firebaseConfig = {
-  apiKey: "AIzaSyA4kGynSyqJmUHzHbuRNPWzDFWHGGT4",
+  apiKey: "AIzaSyAk0y83VxqUCH4JhoDmzxPRbPz0FWi6OT4",
   authDomain: "trip-tracker-rajeh.firebaseapp.com",
   projectId: "trip-tracker-rajeh",
   storageBucket: "trip-tracker-rajeh.appspot.com",
-  messagingSenderId: "1025723412931",
-  appId: "1:1025723412931:web:53a9fa6e1a7a5f43a3dbec",
-  measurementId: "G-J1RBF8H0CC"
+  messagingSenderId: "1025733612933",
+  appId: "1:1025733612933:web:63a9b1eb7a6f35e3a0e0c",
+  measurementId: "G-1S9X5NSZ6Q",
 };
 
-// التهيئة (مرة واحدة فقط)
+// التهيئة
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// تصدير الأدوات اللازمة
-export { 
-    db, 
-    collection, 
-    addDoc, 
-    updateDoc, 
-    doc, 
-    getDocs, 
-    query, 
-    where, 
-    getDoc, 
-    writeBatch,
-    deleteDoc,
-    orderBy 
+// تصدير الدوال
+export {
+  db,
+  collection,
+  addDoc,
+  updateDoc,
+  doc,
+  getDocs,
+  query,
+  where,
+  getDoc,
+  writeBatch,
+  deleteDoc,
+  orderBy,
 };
